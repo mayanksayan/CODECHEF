fca97726d899bbc5b650fdc7e03d8f80144a1313
Mayank Sayan <107642082+mayanksayan@users.noreply.github.com>
2023-02-26 03:57:30 +0530

    Create determine_the_score.cpp

---
+++ determine_the_score.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int n;
+	cin>>n;
+	while(n--){
+	    int a,b;
+	    cin>>a;
+	    cin>>b;
+	    cout<<((a/10)*b)<<endl;
+	}
+	return 0;
+}
