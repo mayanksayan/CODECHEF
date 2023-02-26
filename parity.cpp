76bbc9c3c39a1685ebce5849afb991fb173cfb0e
Mayank Sayan <107642082+mayanksayan@users.noreply.github.com>
2023-02-26 19:26:47 +0530

    Create parity.cpp

---
+++ parity.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int n;
+	cin>>n;
+	while(n--){
+	    int a;
+	    cin>>a;
+	    if(a%2==0)
+	        cout<<"YES\n";
+	    else
+	    cout<<"NO\n";
+	}
+	return 0;
+}
