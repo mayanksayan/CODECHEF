a47b396de46ed8e1d303e496223d92d337766399
Mayank Sayan <107642082+mayanksayan@users.noreply.github.com>
2023-02-26 19:43:28 +0530

    Create battery_health.cpp

---
+++ battery_health.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int n;
+	cin>>n;
+	while(n--){
+	    int a;
+	    cin>>a;
+	    if(a>=80)
+	        cout<<"YES\n";
+	        else
+	        cout<<"NO\n";
+	        
+	}
+	return 0;
+}
