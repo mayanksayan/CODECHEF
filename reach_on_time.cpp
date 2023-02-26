f79ff756ae7e050ccae89ac4bd7fac32d5d22335
Mayank Sayan <107642082+mayanksayan@users.noreply.github.com>
2023-02-26 19:21:07 +0530

    Create reach_on_time.cpp

---
+++ reach_on_time.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int n;
+	cin>>n;
+	while(n--){
+	    int a;
+	    cin>>a;
+	    if(a>=30){
+	        cout<<"YES\n";
+	    }
+	    else{
+	        cout<<"NO\n";
+	    }
+	}
+	return 0;
+}
