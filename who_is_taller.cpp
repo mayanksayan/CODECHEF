d2de04097983be77cc9dd609ec514c683dff1f2e
Mayank Sayan <107642082+mayanksayan@users.noreply.github.com>
2023-02-26 19:07:50 +0530

    Create who_is_taller.cpp

---
+++ who_is_taller.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int n;
+	cin>>n;
+	while(n--){
+	    int a,b;
+	    cin>>a;
+	    cin>>b;
+	    if(a>b){
+	        cout<<"A"<<endl;
+	    }
+	    else{
+	        cout<<"B"<<endl;
+	    }
+	}
+	return 0;
+}
