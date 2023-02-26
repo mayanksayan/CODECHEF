067f794f154337024981af828a59c93858459d79
Mayank Sayan <107642082+mayanksayan@users.noreply.github.com>
2023-02-26 19:23:55 +0530

    Create chef_and_brain_speed.cpp

---
+++ chef_and_brain_speed.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int a,b;
+	cin>>a;
+	cin>>b;
+	if(b>a)
+	    cout<<"YES";
+	else
+	     cout<<"NO";
+	
+	return 0;
+}
