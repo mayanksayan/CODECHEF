d51c1fc29bcddb64e3d68882f1127d2a027faa9f
Mayank Sayan <107642082+mayanksayan@users.noreply.github.com>
2023-02-27 14:23:10 +0530

    Create number_of_credits.cpp

---
+++ number_of_credits.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int n;
+	cin>>n;
+	while(n--){
+	    int a,b,c;
+	    cin>>a>>b>>c;
+	    cout<<((a*4)+(b*2)+(c*0))<<endl;
+	}
+	return 0;
+}
