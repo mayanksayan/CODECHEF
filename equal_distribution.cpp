f6bc9143d2bfe6040e77aafe2ee971f5a85c021e
Mayank Sayan <107642082+mayanksayan@users.noreply.github.com>
2023-02-28 19:06:35 +0530

    Create equal_distribution.cpp

---
+++ equal_distribution.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int n;
+	cin>>n;
+	while(n--){
+	    int a,b;
+	    cin>>a>>b;
+	    if((a+b)%2==0)
+	    cout<<"yes"<<endl;
+	    else
+	    cout<<"no"<<endl;
+	}
+	return 0;
+}
