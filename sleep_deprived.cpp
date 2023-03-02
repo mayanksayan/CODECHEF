f3c0ee6d0455e78155d74dd1ae45f734cae8dd02
Mayank Sayan <107642082+mayanksayan@users.noreply.github.com>
2023-03-02 21:05:41 +0530

    Create sleep_deprived.cpp

---
+++ sleep_deprived.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int n;
+	cin>>n;
+	while(n--){
+	    int a;
+	    cin>>a;
+	    if(a<7)
+	    cout<<"yes"<<endl;
+	    else
+	    cout<<"no"<<endl;
+	}
+	return 0;
+}
