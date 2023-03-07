258932506fbecbbfdcf5e1ebe0115f8921f8d568
Mayank Sayan <107642082+mayanksayan@users.noreply.github.com>
2023-03-07 12:19:00 +0530

    Create read_page.cpp

---
+++ read_page.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int p,q,r,t,s,x,n,y;
+    cin>>t;
+    while(t--)
+    {
+        cin>>n>>x>>y;
+       s=x*y;
+       if(s>=n)
+       {
+           cout<<"YES"<<endl;
+       }
+       else
+       {
+           cout<<"NO"<<endl;
+       }
+    }
+    
+    
+    
+    
+	// your code goes here
+	return 0;
+}
