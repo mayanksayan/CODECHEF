ff0668c929f04237737c772aee71fe574d997e33
Mayank Sayan <107642082+mayanksayan@users.noreply.github.com>
2023-03-07 12:21:18 +0530

    Create lunchtime.cpp

---
+++ lunchtime.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int n;
+	cin>>n;
+	while(n--){
+	    int a;
+	    cin>>a;
+	    if(a>=1 && a<=4)
+	    cout<<"yes"<<endl;
+	    else cout<<"no"<<endl;
+	}
+	return 0;
+}
