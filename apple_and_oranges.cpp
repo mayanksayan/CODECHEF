a6d73a9d7f485c56c70b0de483528d5955a20b22
Mayank Sayan <107642082+mayanksayan@users.noreply.github.com>
2023-03-07 12:25:31 +0530

    Create apple_and_oranges.cpp

---
+++ apple_and_oranges.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+#define t(x) ; int x; cin>>x;while(x--)
+
+void solve(){
+    int a,b,x;
+    cin>>x;
+    cin>>a>>b;
+    if((a+b)<=x)
+    cout<<"YES"<<endl;
+    else
+    cout<<"NO"<<endl;
+
+}
+
+int main() {
+	// your code goes here
+    solve();
+	
+	return 0;
+}
