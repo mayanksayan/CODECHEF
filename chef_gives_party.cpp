dbc40595632d04b56ddc7764cb23b9083655b0e5
Mayank Sayan <107642082+mayanksayan@users.noreply.github.com>
2023-03-11 13:00:12 +0530

    Create chef_gives_party.cpp

---
+++ chef_gives_party.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int n,x,k;
+	    cin>>n>>x>>k;
+	    if((n*x)<=k) cout<<"YES"<<endl;
+	    else cout<<"NO"<<endl;
+	}
+	return 0;
+}
