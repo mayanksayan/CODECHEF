4c9dbf5596592c0c37d9a9378c972607b84a351d
Mayank Sayan <107642082+mayanksayan@users.noreply.github.com>
2023-03-12 11:15:28 +0530

    Create beat_the_average.cpp

---
+++ beat_the_average.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+#define int long long 
+signed main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int x,y,z;
+	    cin>>x>>y>>z;
+	    if(y<=z){
+	        cout<<0<<endl;
+	    }
+	    else{
+	        int a = x*z;
+	     int b = a/(z+1);
+	     cout<<b<<endl;
+	    }
+	    
+	}
+	return 0;
+}
