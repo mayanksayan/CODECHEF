c84398e4835bbf99a01e347b6dd2a25da9088936
Mayank Sayan <107642082+mayanksayan@users.noreply.github.com>
2023-03-16 16:39:09 +0530

    Create minimum_coins.cpp

---
+++ minimum_coins.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;cin>>t;
+    while(t--)
+    {
+        int a;cin>>a;
+        int coin=a%10;
+        cout<<coin<<endl;
+    }
+	// your code goes here
+	return 0;
+}
