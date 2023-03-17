8b35ba1dbb42f44f7a92a2a30408aaa50fa14e82
Mayank Sayan <107642082+mayanksayan@users.noreply.github.com>
2023-03-17 11:37:33 +0530

    Create just one more episode.cpp

---
+++ just one more episode.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int n;
+	cin>>n;
+	while(n--){
+	    int a;
+	    cin>>a;
+	    if(a<=24) cout<<"no"<<endl;
+	    else cout<<"yes"<<endl;
+	   
+	 
+	}
+	return 0;
+}
