dd03b1db4925695409ab3a613ba62ae215fd2de0
Mayank Sayan <107642082+mayanksayan@users.noreply.github.com>
2023-03-28 06:48:07 +0530

    Create is it hot or cold.cpp

---
+++ is it hot or cold.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int n;
+	cin>>n;
+	while(n--){
+	    int a;
+	    cin>>a;
+	    if(a>20) cout<<"HOT"<<endl;
+	    else cout<<"COLD"<<endl;
+	}
+	return 0;
+}
