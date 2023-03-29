9fc7e4017de11aab0ca8b9432b803aafc5320810
Mayank Sayan <107642082+mayanksayan@users.noreply.github.com>
2023-03-29 22:57:14 +0530

    Create candy division.cpp

---
+++ candy division.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int a;
+	cin>>a;
+	while(a--)
+	{ int t;
+	  cin>>t;
+	  t%3==0?cout<<"YES"<<endl:cout<<"NO"<<endl;
+	    
+	}
+	return 0;
+}
