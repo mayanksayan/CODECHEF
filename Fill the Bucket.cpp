4e05604917a2b2fdcf960abc8678d70b62c23a4c
Mayank Sayan <107642082+mayanksayan@users.noreply.github.com>
2023-04-01 18:12:03 +0530

    Create Fill the Bucket.cpp

---
+++ Fill the Bucket.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t,k,x;
+	cin>>t;
+	while(t!=0)
+	{
+	    cin>>k>>x;
+	    cout<<k-x<<endl;
+	    t--;
+	}
+	return 0;
+}
