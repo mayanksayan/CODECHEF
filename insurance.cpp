6ec2213d304273be3f5dac595adb0df8e034ddf2
Mayank Sayan <107642082+mayanksayan@users.noreply.github.com>
2023-04-12 14:27:30 +0530

    Create insurance.cpp

---
+++ insurance.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int n;
+	cin>>n;
+	while(n--){
+	    int a,b;
+	    cin>>a>>b;
+	    if(a>b) cout<<b<<endl;
+	    else cout<<a<<endl;
+	}
+	return 0;
+}
