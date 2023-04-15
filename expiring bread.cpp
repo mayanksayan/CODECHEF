b41cf69a3fe03e1c40fc0fe998dc1de0da616dbf
Mayank Sayan <107642082+mayanksayan@users.noreply.github.com>
2023-04-15 09:08:33 +0530

    Create expiring bread.cpp

---
+++ expiring bread.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int n;
+	cin>>n;
+	while(n--){
+	    int a,b,c;
+	    cin>>a>>b>>c;
+	    if((c*b)>=a) cout<<"yes"<<endl;
+	    else cout<<"no"<<endl;
+	    
+	}
+	return 0;
+}
