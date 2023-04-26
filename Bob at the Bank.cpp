44fb7472693e2aa3db9fcc5346b58e18be3cb5fd
Mayank Sayan <107642082+mayanksayan@users.noreply.github.com>
2023-04-26 14:40:30 +0530

    Create Bob at the Bank.cpp

---
+++ Bob at the Bank.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int n;
+	cin>>n;
+	while(n--){
+	    int a,b,c,d;
+	    cin>>a>>b>>c>>d;
+	    cout<<(a+(b-c)*d)<<endl;
+	}
+	return 0;
+}
