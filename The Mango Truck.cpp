efbfa1048c4efb370539697ba81297514963dd97
Mayank Sayan <107642082+mayanksayan@users.noreply.github.com>
2023-04-29 21:40:58 +0530

    Create The Mango Truck.cpp

---
+++ The Mango Truck.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int n;
+	cin>>n;
+	while(n--){
+	    int a,b,c;
+	    cin>>a>>b>>c;
+	    cout<<(c-b)/a<<endl; 
+	}
+	return 0;
+}
