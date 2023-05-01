7843a2ccb178b39d81598d26dc70a31cf1f17671
Mayank Sayan <107642082+mayanksayan@users.noreply.github.com>
2023-05-01 13:31:15 +0530

    Create Playlist.cpp

---
+++ Playlist.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while (t--){
+        int n,x;
+        cin>>n>>x;
+        int a=n/(3*x);
+        cout<<a<<endl;
+        }
+	return 0;
+}
