8130a1e30d48db1d9d2e5293d148c9532320562e
Mayank Sayan <107642082+mayanksayan@users.noreply.github.com>
2023-05-03 22:35:58 +0530

    Create Four Tickets.cpp

---
+++ Four Tickets.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int n;
+	cin>>n;
+	while(n--){
+	    int a;
+	    cin>>a;
+	    if((a*4)<=1000) cout<<"yes"<<endl;
+	    else cout<<"no"<<endl;
+	}
+	return 0;
+}
