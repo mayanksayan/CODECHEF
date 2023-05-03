b5c0b6065df15bc61787ba7993ce78e563ebcdba
Mayank Sayan <107642082+mayanksayan@users.noreply.github.com>
2023-05-03 22:57:34 +0530

    Create Shopping Change.cpp

---
+++ Shopping Change.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int n;
+	cin>>n;
+	while(n--){
+	    int a;
+	    cin>>a;
+	    cout<<(100-a)<<endl;
+	}
+	return 0;
+}
