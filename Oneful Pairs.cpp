6e2a6f140c72f897c7c05e57bccd617a43ca983d
Mayank Sayan <107642082+mayanksayan@users.noreply.github.com>
2023-06-07 07:41:49 +0530

    Create Oneful Pairs.cpp

---
+++ Oneful Pairs.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int a,b;
+	cin>>a>>b;
+	if(a+b+(a*b)==111) cout<<"yes"<<endl;
+	else cout<<"no"<<endl;
+	return 0;
+}
