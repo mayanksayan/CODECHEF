6552bd98da0a788e72548c20e5a50d34a2c4bf29
Mayank Sayan <107642082+mayanksayan@users.noreply.github.com>
2023-07-31 23:50:29 +0530

    Create Puzzle Hunt.cpp

---
+++ Puzzle Hunt.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int n;
+	cin>>n;
+	if(n>=6 && n<=8)
+	cout<<"yes"<<endl;
+	else cout<<"no"<<endl;
+	return 0;
+}
