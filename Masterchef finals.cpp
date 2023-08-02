58281e406f400133a1c7f419571e58eaaa8c8565
Mayank Sayan <107642082+mayanksayan@users.noreply.github.com>
2023-08-02 16:30:43 +0530

    Create Masterchef finals.cpp

---
+++ Masterchef finals.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int n;
+	cin>>n;
+	while(n--){
+	    int a;
+	    cin>>a;
+	    if(a<=10) cout<<"yes"<<endl;
+	    else cout<<"no"<<endl;
+	}
+	return 0;
+}
