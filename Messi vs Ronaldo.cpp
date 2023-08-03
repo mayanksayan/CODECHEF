6894ae40d4c84d1a404e77dec30ed01c7fb52287
Mayank Sayan <107642082+mayanksayan@users.noreply.github.com>
2023-08-03 23:47:19 +0530

    Create Messi vs Ronaldo.cpp

---
+++ Messi vs Ronaldo.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int a,b,c,d;
+	cin>>a>>b>>c>>d;
+	if((a*2+b)>(c*2+d)) cout<<"messi"<<endl;
+	else if((a*2+b)<(c*2+d)) cout<<"ronaldo"<<endl;
+	else cout<<"equal"<<endl;
+	return 0;
+}
